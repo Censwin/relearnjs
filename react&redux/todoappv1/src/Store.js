f7bdc07a6bbfa2b6b22ea3386f5e47ed65a994ee
Censwin <704334497@qq.com>
2021-02-02 00:04:28 +0800

    add combineReducers

--- react&redux/todoappv1/src/Store.js
+++ react&redux/todoappv1/src/Store.js
@@ -1,4 +1,11 @@
 import {createStore, combineReducers} from 'redux' 
 
+import {reducer as todoReducer} from './todos';
+import {reducer as filterReducer} from './filter'
 
-export default createStore(store)
+const reducer = combineReducers({
+  todos: todoReducer,
+  filter: filterReducer
+})
+
+export default createStore(reducer)
