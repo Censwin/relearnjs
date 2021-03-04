333a9d4214534fb418f77dfa17c8ba97ebb6d4f6
kyle <704334497@qq.com>
2021-03-04 18:44:56 +0800

    update

--- reactandredux/my-app/src/todos/views/addTodo.js
+++ reactandredux/my-app/src/todos/views/addTodo.js
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {Component} from 'react'
 
+class Addtodo extends Component {
 
-const Addtodo = () => { 
-  return (
-    <div>
-      Addtodo
+  Addtodo = () => { 
+    const onSubmit = () => {
+      console.log(123);
+    }
+  }
+  render () {
+    return (
+      <div>
+      <form onSubmit={this.onSubmit}>
+        <input placeholder="please input text" ref={this.refInput} />
+        <button type="submit">
+          添加
+        </button>
+      </form>
     </div>
-  )
+    )
+  }
 }
 
 export default Addtodo
